a0e4d63d6d55e062aae0e3600a95a06ba7948a5d
Denis <dennimail@gmail.com>
2022-04-26 16:32:27 +0300

    feat: burger menu

---
+++ shelter/pages/main/main.js
@@ -0,0 +1,14 @@
+const popupLayout = document.getElementById("shadow")
+const burger = document.querySelector(".burger-container")
+
+    
+function clickFunction(e) {
+   
+        alert(e)
+    }
+    // handleEvent(e) {
+    //     alert(e.type + " на " + e.target);
+    //   }
+    // }
+
+    burger.addEventListener("click", clickFunction)

e8bb81b20709e7901e45b1cb95f7bc601ddc6073
Denis <dennimail@gmail.com>
2022-04-27 12:42:30 +0300

    feat: add burger menu

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,12 +1,26 @@
 const popupLayout = document.getElementById("shadow")
-const burger = document.querySelector(".burger-container")
+const burger = document.querySelector(".burger")
+const burgerMenu = document.querySelector(".burger-menu-off")
+const body = document.body
 
+window.onload = function () {
+    console.log()
+    addBurgerHandler()
+}
+
+const addBurgerHandler = () =>{
+    document.addEventListener("click", (e)=>{
+        if(e.target.classList.contains("slide-away-tag") == true){
+            toggleBurgerMenu()
+        }
+    })
+}
+
+const toggleBurgerMenu = () => {
+    popupLayout.classList.toggle("toggle-menu")
+    burgerMenu.classList.toggle("burger-menu-on")
+    body.classList.toggle("no-scrolling")
+    burger.classList.toggle("burger-fixed")
     
-function clickFunction(e) {
-        popupLayout.classList.toggle("toggle-menu")
+}
 
-    }
-burger.addEventListener("click", clickFunction)
-document.addEventListener("click", (e)=>{
-    alert((e.target.classList))
-})
